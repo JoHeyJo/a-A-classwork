a53a415f272aa24957ef2008d52092f352f34279
jerod <jbrackin1@gmail.com>
2021-11-09 16:52:51 -0600

    added populate

--- D2/board.rb
+++ D2/board.rb
@@ -1,17 +1,38 @@
 class Board 
+    CARDS = [:A, :A, :B, :B, :C, :C, :D, :D, :E, :E, :F, :F, :G, :G, :H, :H]
 
     def initialize
         @grid = Array.new(4) {Array.new(4, ' ')}
     end
 
+    def [](pos) 
+        row, col = pos
+        @grid[row][col]     
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
+    end
+    
     def print
         @grid.each do |row|
             p row
         end
     end
 
-    def populate
-        pair 
+    def empty? 
+        @grid.flatten.any? {|pos| pos == ' ' }
     end
 
+    def populate
+            CARDS.each do |card|
+            row = rand(0...@grid.length)
+            col = rand(0...@grid.length)
+            pos = [row, col]
+            self[pos] = card if self[pos] == " "
+        end
+    end
+
+    
 end
