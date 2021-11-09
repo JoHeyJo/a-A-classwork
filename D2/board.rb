0058d6a040a116d9973713dccf9ee2e0e525e441
jf <jpf0628@gmail.com>
2021-11-09 14:11:42 -0800

    update

--- D2/board.rb
+++ D2/board.rb
@@ -1,4 +1,17 @@
 class Board 
 
     def initialize
-        
+        @grid = Array.new(4) {Array.new(4, ' ')}
+    end
+
+    def print
+        @grid.each do |row|
+            p row
+        end
+    end
+
+    def populate
+        pair 
+    end
+
+end
