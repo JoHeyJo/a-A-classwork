1e63b501763908b51bdbf5e06ba4f19143ad3f1d
jf <jpf0628@gmail.com>
2021-11-08 16:12:08 -0800

    arrays

--- week3/enumerables.rb
+++ week3/enumerables.rb
@@ -65,9 +65,29 @@ class Array
       end
       return new_arr
     end
+
+    def my_zip(*arrays)
+      new_arr = Array.new(self.length) {Array.new(arrays.length + 1)}
+      arrays.unshift(self)
+      # p arrays
+      (0...self.length).each do |i|
+        (0...arrays.length).each do |j|
+          new_arr[i][j] = arrays[j][i]
+        end
+        end
+        return new_arr
+    end
 end
 
-    
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 
 # return_value = [1, 2, 3].my_each do |num|
@@ -100,3 +120,4 @@ end
 # p a.my_all? { |num| num < 4 } # => true
   
 # p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+
