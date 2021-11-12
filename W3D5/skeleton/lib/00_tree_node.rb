80c86e55de7433f56dd8b0d565f75d79b89b0437
jf <jpf0628@gmail.com>
2021-11-12 14:51:08 -0800

    confused

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,27 @@
 class PolyTreeNode
 
+    attr_reader :value, :children
+    attr_accessor :parent
+
+    def initialize(value= nil)
+        @value = value
+        @parent = nil #parent node
+        @children = []
+    end
+
+    def parent=(parent)
+        return if self.parent == parent 
+        # detach from the current parent
+        if self.parent
+            self.parent._children.delete(self)
+        end
+        @parent = parent
+
+
+
+
+        # @parent.children << new_parent
+        # new_parent.children = children
+    end
+
 end
