5ba9ab84308f2717bfbdf4e83120247d0d3f1595
Charlie Sheppard <sheppard.charlie24@gmail.com>
2021-11-12 16:02:23 -0800

    Add initialize to #parent=

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -3,25 +3,65 @@ class PolyTreeNode
     attr_reader :value, :children
     attr_accessor :parent
 
-    def initialize(value= nil)
+    def initialize(value)
         @value = value
-        @parent = nil #parent node
         @children = []
+        @parent = nil #parent node
     end
+
+    # def parent=(parent)
+    #     # self.parent is the previous parent
+    #     # parent == the arg -> new parent
+    #     return if self.parent == parent
+
+    #     # First, detach from current parent.
+    #     # we are detaching from the current parent's children so that we can become the parent
+    #     if self.parent
+    #     self.parent._children.delete(self)
+    #     end
+
+    #     # No new parent to add this to.
+    #     # self becomes a child of the new parent
+    #     @parent = parent
+    #     self.parent._children << self unless self.parent.nil?
+
+    #     self
+    # end
+
+    # def initialize(value = nil)
+    #     @value, @parent, @children = value, nil, []
+    # end
+
+    # def children
+    #     # We dup to avoid someone inadvertently trying to modify our
+    #     # children directly through the children array. Note that
+    #     # `parent=` works hard to make sure children/parent always match
+    #     # up. We don't trust our users to do this.
+    #     @children.dup
+    # end
 
     def parent=(parent)
-        return if self.parent == parent 
-        # detach from the current parent
+        # self.parent is the previous parent
+        # parent == the arg -> new parent
+        return if self.parent == parent
+
+        # First, detach from current parent.
+        # we are detaching from the current parent's children so that we can become the parent
         if self.parent
-            self.parent._children.delete(self)
+            self.parent.children.delete(self)
         end
+
+        # No new parent to add this to.
+        # self becomes a child of the new parent
         @parent = parent
+        self.parent.children << self unless self.parent.nil?
 
-
-
-
-        # @parent.children << new_parent
-        # new_parent.children = children
+        self
     end
 
+
+
 end
+
+# t = PolyTreeNode.new
+# p t
