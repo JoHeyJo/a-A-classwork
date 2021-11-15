fdd6aac661415686d7e37d39d9acbe40a0fa9ea9
jf <jpf0628@gmail.com>
2021-11-15 15:33:30 -0800

    valid moves

--- W3D5/knights_travails/knights_travails.rb
+++ W3D5/knights_travails/knights_travails.rb
@@ -3,14 +3,32 @@ require_relative 'poly_tree_node'
 class KnightPathFinder
 
     attr_reader :pos
-
-    def self.root_node(pos)
-        PolyTreeNode.new(pos)    
+    @@possible_moves = [[1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,1], [-1,2]]
+    
+    def self.root_node
+        # PolyTreeNode.new(pos)    
     end
 
+    def self.valid_moves(pos)
+
+        @@possible_moves.each do |move|
+            move_0 = pos[0] + move[0] 
+            move_1 = pos[1] + move[1]
+            if (move_0 >= 0 && move_0 < 8) && (move_1 >= 0 && move_1 < 8)
+                return [move_0, move_1]
+            end
+        end
+    end
+        
+
     def initialize(pos)
+        @considered_positions = [pos]
         @pos = pos
         self.build_move_tree
+    end
+
+    def new_move_positions(pos)
+        @considered_positions << KnightPathFinder.valid_moves(pos) if !@considered_positions.include?(KnightPathFinder.valid_moves(pos))
     end
 
     def find_path(pos)
