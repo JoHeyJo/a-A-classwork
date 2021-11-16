e3a57a999305ecc7de97615bb980b25546a25d24
hedwards <hgedwards87@gmail.com>
2021-11-16 14:22:06 -0800

    started employee and manager initialize

---
+++ W4D2/employees/employee.rb
@@ -0,0 +1,29 @@
+class Employee
+    attr_reader :name, :title, :salary, :boss
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary   
+        @boss = Manager.new(boss)
+        @boss.employees << self
+    end
+
+    def bonus(num) 
+        @salary * num
+    end
+
+
+end
+
+
+
+class Manager < Employee
+    attr_accessor :employees
+
+    def initialize(name)
+        @name = name
+        @employees = []
+    end
+
+end
