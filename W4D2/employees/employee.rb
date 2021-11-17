cf0325153ed95739518708cdbbaa6f2ba406dd67
jf <jpf0628@gmail.com>
2021-11-16 17:02:31 -0800

    Finished

--- W4D2/employees/employee.rb
+++ W4D2/employees/employee.rb
@@ -1,15 +1,16 @@
 class Employee
     attr_reader :name, :title, :salary, :boss
 
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary, boss=nil)
         @name = name
         @title = title
         @salary = salary   
         @boss = boss
+        self.boss = boss
     end
 
     def boss=(boss)
-        # @boss = boss
+        @boss = boss
         boss.add_employee(self) unless boss == nil 
     end
 
@@ -25,7 +26,7 @@ end
 class Manager < Employee
     attr_accessor :employees
 
-    def initialize(name, title, salary, boss)
+    def initialize(name, title, salary, boss=nil)
         super
         @employees = []
     end
@@ -40,15 +41,23 @@ class Manager < Employee
     end
 
     def bfs
-        total = 
+        total = -self.salary
         queue = [self]
 
         until queue.empty?
             node = queue.shift
-            queue.concat(node.employees)
+            queue.concat(node.employees) if node.is_a?(Manager) 
             total += node.salary
         end
         total 
     end
 
 end
+
+ceo = Manager.new('Ned','founder', 1000000)
+manager = Manager.new('Darren', 'founder', 78000, ceo)
+employee_1  = Employee.new('david', 'TA', 10000, manager)
+employee_2 = Employee.new('shawna', 'TA', 12000, manager)
+
+p manager.bonus(4)
+p ceo.bonus(5)
