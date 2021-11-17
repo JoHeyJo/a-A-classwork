c3612af95ea347ed1594c31d1965787f8e0ae4e7
hedwards <hgedwards87@gmail.com>
2021-11-16 17:31:47 -0800

    started chess board

---
+++ W4D2/chess/board.rb
@@ -0,0 +1,57 @@
+require_relative 'piece'
+
+class Board
+    
+    def initialize
+        @grid = Array.new(8) {Array.new(8)}
+        @null_piece = NullPiece
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+
+    def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
+    end
+
+    def move_piece(color, start_pos, end_pos)
+
+    end
+
+    def valid_pos?(pos)
+        pos.map.all? { |n| (0...@grid.length).include?(n) }
+    end
+
+    def add_piece(piece, pos)
+        self[pos] = piece
+    end
+    
+    def checkmate?(color)
+
+    end
+
+    def in_check?(color)
+
+    end
+
+    def find_king(color)
+
+    end
+
+    def pieces
+
+    end
+
+    def dup
+
+    end
+
+    def move_piece!(color, start_pos, end_pos)
+
+    end
+
+end
