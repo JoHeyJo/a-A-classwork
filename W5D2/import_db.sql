9e3632e42cb388fcba47811f6e1e2da582c86eb3
jf <jpf0628@gmail.com>
2021-11-23 11:45:25 -0800

    making tables

---
+++ W5D2/import_db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users {
+    
+    id INTEGER PRIMARY KEY,
+    fname VARCHAR NOT NULL,
+    lname VARCHAR NOT NULL,
+
+}
+
+CREATE TABLE questions {
+    id INTEGER PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    body VARCHAR NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+
+
+}
+
+CREATE TABLE question_follows {
+    id INTEGER PRIMARY KEY,
+    follower_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (follower_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+    
+}
