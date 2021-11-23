512e0d373a020de48fd3b9be64c47f42d0387c95
Jack Tomasik <jackedwardtomasik@gmail.com>
2021-11-23 12:15:55 -0800

    first few tables

--- W5D2/import_db.sql
+++ W5D2/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users {
     
     id INTEGER PRIMARY KEY,
@@ -25,3 +27,27 @@ CREATE TABLE question_follows {
     FOREIGN KEY (question_id) REFERENCES questions(id)
     
 }
+
+CREATE TABLE replies {
+    id INTEGER PRIMARY KEY
+    body VARCHAR NOT NULL
+    question_reference_id INTEGER KEY
+    parent_question_reference_id INTEGER KEY
+    user_reference_id INTEGER KEY
+    
+
+    FOREIGN KEY (question_reference_id) REFERENCES questions(id)
+    FOREIGN KEY (parent_question_reference_id) REFERENCES replies(id)
+    FOREIGN KEY (user_reference_id) REFERENCES users(id)
+}
+
+CREATE TABLE question_likes {
+    liked BOOLEAN NOT NULL
+    user_reference_id INTEGER NOT NULL
+    question_reference_id INTEGER NOT NULL
+
+    FOREIGN KEY (like_reference_id) REFERENCES users(id)
+    FOREIGN KEY (question_reference_id) REFERENCES questions(id)
+}
+
+
