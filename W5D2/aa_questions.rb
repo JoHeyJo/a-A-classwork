5b7e3784817820e1a5b4ba40fc57e85e6ca99232
jf <jpf0628@gmail.com>
2021-11-23 15:48:56 -0800

    find id

--- W5D2/aa_questions.rb
+++ W5D2/aa_questions.rb
@@ -12,7 +12,29 @@ class QuestionsDatabase < SQLite3::Database
 
 end
 
+class Question
+
+    def self.all    
+        data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
+        data.map { |datum| Question.new(datum) }
+    end
+
+    def self.find_by_id
+        
+    end
+    
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        user_id = options['user_id']
+    end
+
+
+end
+
 class Users
+
 
     def initialize(options)
         @id = options['id']
