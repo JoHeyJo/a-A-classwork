08c3e4a00df91896b3b8c8f218db13a693e3bd77
Jack Tomasik <jackedwardtomasik@gmail.com>
2021-11-23 16:38:23 -0800

    classes and methods for questions and users

--- W5D2/aa_questions.rb
+++ W5D2/aa_questions.rb
@@ -2,38 +2,59 @@
     require 'singleton'
 
 class QuestionsDatabase < SQLite3::Database
-    inlcude Singleton
+    include Singleton
 
     def initialize
         super('./questions.db')
-        self.type_translate = true
+        self.type_translation = true
         self.results_as_hash = true
     end
 
 end
 
 class Question
+    attr_accessor :id, :title, :body, :user_id
 
     def self.all    
         data = QuestionsDatabase.instance.execute("SELECT * FROM questions")
         data.map { |datum| Question.new(datum) }
     end
 
-    def self.find_by_id
-        
+    def self.find_by_id(id)
+        question = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM questions
+            WHERE id = ?
+
+            SQL
     end
     
     def initialize(options)
         @id = options['id']
         @title = options['title']
         @body = options['body']
-        user_id = options['user_id']
+        @user_id = options['user_id']
     end
 
 
 end
 
 class Users
+    attr_accessor :id, :fname, :lname
+
+    def self.all    
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+        data.map { |datum| Users.new(datum) }
+    end
+
+    def self.find_by_id(id)
+        user = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM users
+            WHERE id = ?
+
+            SQL
+    end
 
 
     def initialize(options)
@@ -44,11 +65,13 @@ class Users
     end
 
     def self.find_by_name(fname, lname)
-    
+        user = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+            SELECT *
+            FROM users
+            WHERE fname = ? AND lname = ?
+
+            SQL
     end
 
-    def self.fname
-
-    end
 
 end
