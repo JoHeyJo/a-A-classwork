98b0ffa30adfb58e74e9163a4de9a4b138d6fd46
arleen <arleenpandher11@gmail.com>
2021-11-29 19:55:36 -0500

    V2

--- W6D1/movie_buff_project/movie_buff/02_queries.rb
+++ W6D1/movie_buff_project/movie_buff/02_queries.rb
@@ -3,11 +3,20 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where("yr BETWEEN 1980 AND 1989")
+    .where(score: 3..5)
+
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
 
+  Movie
+    .group(:yr)
+    .where("score < 8")
+    .pluck(:yr)
 end
 
 def cast_list(title)
