e33fd8ca0d486608b4bc5fbad4f8071cea59847a
jf <jpf0628@gmail.com>
2021-12-01 15:44:32 -0800

    setup rails server

--- W6D1/movie_buff_project/movie_buff/02_queries.rb
+++ W6D1/movie_buff_project/movie_buff/02_queries.rb
@@ -22,7 +22,7 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-
+  
 end
 
 def vanity_projects
