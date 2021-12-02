b5adaac61e43f6a449ed1a7a27ec9d39a9887ef8
thiago <17tawi@gmail.com>
2021-12-01 17:33:18 -0800

    user_controller show method

--- W6D3/RoutesAndControllers/frac2/app/controllers/users_controller.rb
+++ W6D3/RoutesAndControllers/frac2/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-  
+  def index
+    users = User.all 
+    render json: users 
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+     render json: user
+    else
+     render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
