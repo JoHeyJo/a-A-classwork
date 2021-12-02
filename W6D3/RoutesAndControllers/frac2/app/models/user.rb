dad3f41fd10d357172e4d641eff0efd2575140d3
jf <jpf0628@gmail.com>
2021-12-02 14:35:28 -0800

    working on artworkshare

--- W6D3/RoutesAndControllers/frac2/app/models/user.rb
+++ W6D3/RoutesAndControllers/frac2/app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
     validates :username, uniqueness: true
+
+    has_many :artworks,
+        foreign_key: :artist_id,
+        class_name: :Artwork
+
+    has_many :shared_artworks,
+        foreign_key: :view_id,
+        class_name: :ArtworkShare
 end
