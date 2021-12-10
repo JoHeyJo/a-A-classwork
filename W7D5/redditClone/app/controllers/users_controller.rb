1b50ebc9ad1f206b268fc7a5db4fff616bba0841
BrianKo <bako2466@gmail.com>
2021-12-10 11:26:40 -0800

    finish phase1

---
+++ W7D5/redditClone/app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  before_action :require_logged_in, only:[:destroy, :edit, :show, :update]
+
+  def index 
+    @users = User.all 
+    render :index
+  end 
+
+  def show 
+    @user = User.find(params[:id])
+    render :show
+  end
+  
+  #find returns undefined - breaks code
+  #find_by returns nil if not found
+
+  def new 
+    @user = User.new 
+    render :new
+  end 
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save  
+      redirect_to user_url(@user.id)
+    else  
+      render :new
+    end
+  end 
+
+  def edit 
+    @user = User.find(params[:id]) 
+    render :edit
+  end 
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_url(@user.id)
+    else  
+      redirect_to user_url(@user.id)
+    end
+  end
+
+  def destroy 
+    logout 
+    redirect_to new_session_url
+  end 
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
